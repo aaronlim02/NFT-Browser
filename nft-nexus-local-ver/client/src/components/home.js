1c67cc93c09883d1c609cd9083cd564178929cde
aaronlim02 <e1123168@u.nus.edu>
2024-07-26 19:54:49 +0800

    psod

--- nft-nexus-local-ver/client/src/components/home.js
+++ nft-nexus-local-ver/client/src/components/home.js
@@ -4,6 +4,7 @@ import { searchCollection } from '../utils/api';
 const Home = () => {
   const [output, setOutput] = useState([]);
   const [status, setStatus] = useState(null);
+  const [isPythonInstalled, setIsPythonInstalled] = useState('true');
   const fetchDataCalledRef = useRef(false);
 
   const loadFeaturedNFT = useCallback(async () => {
@@ -14,6 +15,7 @@ const Home = () => {
       console.log('Processed data:', response);
     } catch (error) {
       setStatus('500');
+      setIsPythonInstalled(false);
       console.error('Error fetching collections:', error);
     }
   }, []);
@@ -34,27 +36,51 @@ const Home = () => {
     return urlObj.toString();
   }
 
-  return (
-    <main>
-      <h1 align="center">NFT Nexus</h1>
-      <p align="center">Find NFTs easily using this intuitive website</p>
-      <div class="homepage-featured">
-      {output.length > 0 && output[0].length > 5 && (
-        <div className="image-container">
-          <img src={scaleImgUrl(output[0][5], 1920)} alt="Collection Image" />
-          <div className="overlay">
-            <p class="collection-title">{output[0][0]}</p>
-            <p>#1 NFT in volume</p>
-            <p>({Math.round(output[0][7][1].volume)} ETH within the past 7 days)</p>
-            <p>Price: {output[0][6].floor_price} ETH onwards</p>
-            <a href={output[0][4]}>Get it on Opensea</a>
+  if (isPythonInstalled) {
+
+    return (
+      <main>
+        <h1 align="center">NFT Nexus</h1>
+        <p align="center">Find NFTs easily using this intuitive website</p>
+        <div class="homepage-featured">
+        {output.length > 0 && output[0].length > 5 && (
+          <div className="image-container">
+            <img src={scaleImgUrl(output[0][5], 1920)} alt="Collection Image" />
+            <div className="overlay">
+              <p class="collection-title">{output[0][0]}</p>
+              <p>#1 NFT in volume</p>
+              <p>({Math.round(output[0][7][1].volume)} ETH within the past 7 days)</p>
+              <p>Price: {output[0][6].floor_price} ETH onwards</p>
+              <a href={output[0][4]}>Get it on Opensea</a>
+            </div>
+          </div>
+        )}
+        </div>
+        
+      </main>
+    );
+
+  } else {
+
+    return (
+      <main>
+        <h1 align="center">NFT Nexus</h1>
+        <p align="center">Find NFTs easily using this intuitive website</p>
+        <div class="python-screen-of-death">
+          <div class="psod-message">
+            <h2>PYTHON SCREEN OF DEATH :(</h2>
+            <p>If you are seeing this screen, it means that Python is not installed properly in this application. Python is required for most of the features in this application to function.</p>
+            <h3>What to do:</h3>
+            <p>If you haven't installed Python yet, please do so <a class="psod-a" href="https://www.python.org/downloads/">here.</a> Afterwards, reinstall this application and repeat all the steps in the <a class="psod-a" href="https://github.com/aaronlim02/NFT-Browser">README.</a></p>
           </div>
         </div>
-      )}
-      </div>
-      
-    </main>
-  );
+        
+      </main>
+    );
+
+  }
+
+  
 };
 
 export default Home;
