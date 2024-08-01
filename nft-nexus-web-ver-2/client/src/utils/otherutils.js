18b32062a92786551d60628344a05ac1f65ae912
aaronlim02 <e1123168@u.nus.edu>
2024-08-01 12:33:21 +0800

    cleanup

--- nft-nexus-web-ver-2/client/src/utils/otherutils.js
+++
@@ -1,8 +0,0 @@
-export const isValidWalletAddress = (address) => {
-    if (!address || address.length !== 42) {
-      return false;
-    } else {
-      const hexPattern = /^0x[0-9a-fA-F]+$/;
-      return hexPattern.test(address);
-    }
-  };
