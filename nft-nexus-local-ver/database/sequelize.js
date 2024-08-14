3ee56899832f6d5aba978162362c51c54280d1e1
aaronlim02 <e1123168@u.nus.edu>
2024-08-14 20:19:28 +0800

    for release

--- nft-nexus-local-ver/database/sequelize.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database/database.sqlite', // Path to your SQLite database file
-});
-
-module.exports = sequelize;
